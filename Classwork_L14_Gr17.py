243b4d0b31dfeb69e41864d18d5740f332f94cdd
LikaSv <45724202+LikaSv@users.noreply.github.com>
2023-12-29 16:21:28 +0400

    avtvirte faili

---
+++ Classwork_L14_Gr17.py
@@ -0,0 +1,17 @@
+# import my_module
+# print(my_module.avg(3, 6))
+
+# import pkg.mod1
+# print(pkg.mod1.avg(1,4))
+
+# from pkg import mod1
+# print(mod1.avg(1,4))
+
+# from pkg.mod1 import *
+# print(avg(1,4))
+
+
+# from dateutil.easter import *
+# print(easter(2024, 2))
+
+
